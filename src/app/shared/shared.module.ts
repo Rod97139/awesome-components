cfac94d43d4555ecc4fbbfbec4968defd3c72379
Rod97139 <fayaflame@gmail.com>
2023-10-09 20:06:32 +0200

    init project

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatToolbarModule
+  ]
+})
+export class SharedModule { }
