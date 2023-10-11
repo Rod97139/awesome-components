831abebf9651f727bebe9fb3fa785c9f627468b8
Rod97139 <fayaflame@gmail.com>
2023-10-12 01:25:09 +0200

    transition from void

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -24,6 +24,18 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       ]),
       transition('active => default', [
         animate('500ms ease-in-out')
+      ]),
+      transition('void => *', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+          backgroundColor: 'rgb(201, 157, 242)'
+        }),
+        animate('250ms ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+          backgroundColor: 'white'
+        }))
       ])
     ])
   ]
