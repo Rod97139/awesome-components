8ea9bebbaafe4293ad96b4695d884976a787891f
Rod97139 <fayaflame@gmail.com>
2023-10-12 01:05:08 +0200

    implement animation

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -34,12 +34,15 @@ export class CommentsComponent implements OnInit {
   @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
+  animationStates: { [key: number]: 'default' | 'active' } = {}; // => dictionary
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
-
+    for (let index in this.comments) {
+      this.animationStates[index] = 'default';
+    }
   }
 
   onLeaveComment() {
@@ -48,5 +51,13 @@ export class CommentsComponent implements OnInit {
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+  }
+
+  onListItemMouseEnter(index: number) {
+    this.animationStates[index] = 'active';
+  }
+
+  onListItemMouseLeave(index: number) {
+    this.animationStates[index] = 'default';
   }
 }
